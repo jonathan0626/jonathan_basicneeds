706cee79ca1a05fe9bd8c696a82fc4be2563fe2e
Jonathan Peng <67518082+jonathan0626@users.noreply.github.com>
2020-10-24 19:16:33 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -17,13 +17,13 @@ Config.useanimation = true
 
 -----------------------------------------------------------
 
---補血程度(範圍 0.01 - 0.99)  若設定為負值應會導致無作用
+--補血程度(範圍 0 - 100)  若設定為負值應會導致無作用
 --The extent of the blood supplementation(Range from 0.01 to 0.99).
---滿血是100滴 ，假設設定 0.1 即代表一次回復 100*0.1 = 10滴血
---For example : Suppose you set 0.1 ，this means you will reply to 10% of the max health.
+--滿血是100滴 ，假設設定 10 即代表一次回復 100*0.1 = 10滴血
+--For example : Suppose you set 10 ，this means you will reply to 10% of the max health.
 
-Config.heal1 = 0.1 --第一個補血藥品 The first blood-replenishing drug.
-Config.heal2 = 0.7 --第二個補血藥品 The second blood-replenishing drug.
+Config.heal1 = 10 --第一個補血藥品 The first blood-replenishing drug.
+Config.heal2 = 50 --第二個補血藥品 The second blood-replenishing drug.
 
 -----------------------------------------------------------
 
@@ -31,7 +31,7 @@ Config.heal2 = 0.7 --第二個補血藥品 The second blood-replenishing drug.
 --The extent of the shield value supplementation(Range from 0 to 100).
 --護盾滿的值是100，假設設定25 即代表補了1/4的護盾
 --For example : Suppose you set 25 ，this means you will reply to 25% of the max shield value.
-Config.heal3 = 25
+Config.heal3 = 10
 Config.heal4 = 50
 
 ------------------------------------------------------------
